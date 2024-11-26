406bf7e9f29cb0f95b341f057de248821826ac9d
alecq031723 <144410466+alecq031723@users.noreply.github.com>
2024-11-25 22:22:32 -0500

    add vercel.json

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 06
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+ *  assignment has been copied manually or electronically from any other source (including web sites) or
+ *  distributed to other students.
+ *
+ *  Name: Alec Josef Serrano
+ *  Student ID: 133592238
+ *  Date: November 26,2024
+ *
+ *  Vercel App (Deployed) Link: _____________________________________________________
+ *
+ ********************************************************************************/
+
 const express = require('express');
 const passport = require('passport');
 const cors = require('cors');
